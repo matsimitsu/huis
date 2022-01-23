54caf9ac728e2ba13b0b17000aa18892ef0a5a11
Robert Beekman <robert@matsimitsu.nl>
2022-01-23 12:03:14 +0100

    Fix styling

---
+++ src/lib/components/Tag.svelte
@@ -0,0 +1,33 @@
+<script>
+	export let tag = {};
+
+	let classes = ['mr-1 rounded rounded-full px-2 py-1 text-xs'];
+	switch (tag.color) {
+		case 'brown':
+			classes.push('text-amber-500 bg-amber-50');
+			break;
+		case 'yellow':
+			classes.push('text-yellow-500 bg-yellow-50');
+			break;
+		case 'blue':
+			classes.push('text-blue-500 bg-blue-50');
+			break;
+		case 'red':
+			classes.push('text-red-500 bg-red-50');
+			break;
+		case 'green':
+			classes.push('text-green-500 bg-green-50');
+			break;
+		case 'purple':
+			classes.push('text-purple-500 bg-purple-50');
+			break;
+		case 'pink':
+			classes.push('text-pink-500 bg-pink-50');
+			break;
+		default:
+			classes.push('text-gray-500 bg-gray-50');
+			break;
+	}
+</script>
+
+<span class={classes.join(' ')}>{tag.name}</span>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,39 +23,49 @@
 	import Text from '$lib/components/Text.svelte';
 	import Block from '$lib/components/Block.svelte';
 	import FormattedDate from '$lib/components/FormattedDate.svelte';
+	import Tag from '$lib/components/Tag.svelte';
 
 	export let posts = [];
 </script>
 
-<div class="mx-auto px-4 max-w-2xl py-24">
-	<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl text-center">
-		<span class="mx-auto block xl:inline">Bouwnummer 9</span>
-		<span class="mx-auto block text-teal-600 xl:inline">Updates</span>
+<div class="mx-auto px-4 max-w-6xl py-24">
+	<h1 class="text-5xl tracking-tight font-extrabold text-slate-900 text-center">
+		<span class="mx-auto block md:inline">Bouwnummer 9</span>
+		<span class="mx-auto block text-teal-600 md:inline">Updates</span>
 	</h1>
-	{#each posts as post (post.page.id)}
-		<article class="p-5 mt-8 border-b rounded bg-white">
-			<header>
-				<h1 class="text-4xl font-bold text-gray-800">
-					<Text text={post.page.properties.Name.title} />
-				</h1>
-				<div class="mt-1 text-sm flex items-center">
-					<p class="text-gray-600">
-						<FormattedDate
-							date={post.page.properties?.Date?.date?.start || post.page.created_time}
-						/>
-					</p>
-					{#each post.page.properties.Tags.multi_select as tag}
-						<span class="ml-1 rounded rounded-full bg-gray-200 text-gray-500 px-2 py-1 text-xs"
-							>{tag.name}</span
-						>
-					{/each}
+	<main class="mt-12 mx-auto p-6 pb-12 max-w-4xl text-base leading-relaxed">
+		{#each posts as post (post.page.id)}
+			<article class="py-6 md:grid md:grid-flow-col md:grid-cols-4 border-t mt-12">
+				<div
+					class="hidden md:flex md:col-start-1 md:row-start-1 md:items-center md:self-start md:mt-3 md:text-md md:space-x-1 text-slate-600"
+				>
+					<FormattedDate date={post.page.properties?.Date?.date?.start || post.page.created_time} />
 				</div>
-			</header>
-			<div class="prose mt-8">
-				{#each post.blocks as block (block.id)}
-					<Block {block} />
-				{/each}
-			</div>
-		</article>
-	{/each}
+				<div class="md:col-span-3 md:col-start-2">
+					<header>
+						<h1
+							class="mt-4 text-slate-800 text-4xl font-extrabold tracking-tight leading-tight md:mt-0"
+						>
+							<Text text={post.page.properties.Name.title} />
+						</h1>
+						<div class="mt-1 text-sm flex items-center">
+							<span class="inline md:hidden mr-1">
+								<FormattedDate
+									date={post.page.properties?.Date?.date?.start || post.page.created_time}
+								/>
+							</span>
+							{#each post.page.properties.Tags.multi_select as tag}
+								<Tag {tag} />
+							{/each}
+						</div>
+					</header>
+					<div class="prose mt-8">
+						{#each post.blocks as block (block.id)}
+							<Block {block} />
+						{/each}
+					</div>
+				</div>
+			</article>
+		{/each}
+	</main>
 </div>
